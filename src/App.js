af8a3b08b66081a2416740c41ca98991f0530bec
Kamola Kobilova <kamola_8880@mail.ru>
2023-12-16 20:28:12 +0500

    postMutation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import {useQuery, useMutation} from '@tanstack/react-query'
+
+const POSTS = [
+  {id: 1, title: "POST 1"},
+  {id: 2, title: "POST 2"}
+]
 
 function App() {
-  return (
-    <div className="App">
-   dfsd
-    </div>
-  );
-}
+  console.log(POSTS);
+ const  postQuery = useQuery ({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]) 
+  })
 
+   const newPostMutation = useMutation({
+    mutationFn: title => {
+      return wait(1000).then(() => 
+       POSTS.push({id: crypto.randomUUID(), title})
+      )
+    },
+   })
+
+
+ if (postQuery.isLoading) return <h1>Loading ...</h1>
+ if (postQuery.isError) {
+  return <pre>{JSON.stringify(postQuery.error)}</pre>
+ }
+
+  return <div>
+    {postQuery.data.map(post => (
+      <div key={post.id}>{post.title}</div>
+    ))}
+      <button  disabled={newPostMutation.isLoading}
+       onClick={() => newPostMutation.mutate('new Post')}>
+         Add new
+      </button>
+    </div>
+}
 export default App;
+
+function wait(duration){
+  return new Promise(resolve => setTimeout(resolve, duration))
+}
+ 
--- src/components/SuperHeroes.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const SuperHeroes = () => {
-  return (
-    <div>SuperHeroes</div>
-  )
-}
